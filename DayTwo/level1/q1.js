022c67465f8687efa3d71395ac8833ed0f14803b
Mashnjogu <Dnjogu47@gmail.com>
2022-06-02 16:01:50 +0300

    DayOne of the JS challenge

---
+++ DayTwo/level1/q1.js
@@ -0,0 +1,15 @@
+let challenge = "30 Days of Javascript";
+let companies = 'Facebook, Google, Amazon, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(challenge);
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substr(0,2))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+console.log(companies.split(','))
+console.log(challenge.replace('Javascript', 'Python'))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt('J'))
+console.log(challenge.indexOf('a'))
