35254e7650fc9d0930d7dbd156117ebbc13e30f1
Mashnjogu <Dnjogu47@gmail.com>
2022-06-22 14:21:04 +0300

    Start of Day 6

---
+++ DaySix/level1/q1.js
@@ -0,0 +1,43 @@
+// Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+for(var i = 0; i<= 10; i++){
+    console.log(i)
+}
+
+let x = 0
+console.log("while loop")
+while(x <= 10){
+   
+    console.log(x)
+    x++
+}
+
+let c = 0
+console.log("do while")
+do{
+    console.log(c)
+    c++
+}while(c <= 10)
+
+// Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+console.log("decrease")
+for(var i = 10; i >= 0; i--){
+    console.log(i)
+}
+
+let a = 10
+console.log("while loop")
+while(a >= 0){
+   
+    console.log(a)
+    a--
+}
+
+let d = 10
+console.log("do while")
+do{
+    console.log(d)
+    d--
+}while(d >= 0)
+// Iterate 0 to n using for loop
